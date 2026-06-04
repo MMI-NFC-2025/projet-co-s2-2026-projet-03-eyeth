2c8ef846feddd421e90bc59993a10e030e3248dc
AglaeNcl <aglaencl@gmail.com>
2026-06-04 08:17:45 +0200

    trucs pour que netlify fonctionne

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
+import netlify from '@astrojs/netlify/functions';
 
 export default defineConfig({
     vite: {
         plugins: [tailwindcss()],
     },
     output: 'server',
+    adapter: netlify(),
 });
+
+
